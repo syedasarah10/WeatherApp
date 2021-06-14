80bf5e75a78927b2ab2237acc3403cb7358dbee3
Syeda Sarah <syeda.sarah@ajackus.com>
2021-06-14 20:09:47 +0530

    used import

---
+++ app.js
@@ -0,0 +1,28 @@
+const key = 'IvPdwpRySuANGGdlKPZscCcOw57G7CFL';
+
+export const getWeather = async(id) => {
+    const url = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(url + query);
+    const data = await response.json();
+
+    return data[0];
+}
+
+
+export const getCity = async(city) =>{
+    const url = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+
+    const response = await fetch(url + query);
+    const data = await response.json();
+
+    return data[0];
+
+};
+
+
+
+
