e51bab0a23719506bedab9d99796878e24db4296
Syeda Sarah <syeda.sarah@ajackus.com>
2021-06-16 18:53:02 +0530

    updated readme and variable names

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-import {getWeather} from './app.js'
-import {getCity} from './app.js'
-const cityForm = document.querySelector('form');
-const card = document.querySelector('.card');
-const details = document.querySelector('.details');
-const time = document.querySelector('img.time');
-const icon = document.querySelector('.icon img');
-
+import { getWeather } from "./app.js";
+import { getCity } from "./app.js";
+const cityForm = document.querySelector("form");
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
+const time = document.querySelector("img.time");
 
 const updateUI = (data) => {
-    const{cityDetails, weather } = data;
-    console.log(cityDetails);
-    console.log(weather);
-    
+  const { cityDetails, weather } = data;
 
-    details.innerHTML = `
+  details.innerHTML = `
           <h5 class="my-3">${cityDetails.EnglishName}</h5>
           <div class="my-3">${weather.WeatherText}</div>
           <div class="display-4 my-4">
@@ -22,42 +17,32 @@ const updateUI = (data) => {
           </div>
           `;
 
+  let imageBasedOnTimeofDay = null;
+  if (weather.IsDayTime) {
+    imageBasedOnTimeofDay = "img/day.svg";
+  } else {
+    imageBasedOnTimeofDay = "img/night.svg";
+  }
+  time.setAttribute("src", imageBasedOnTimeofDay);
 
-
-          let imageBasedOnTimeofDay = null;
-          if(weather.IsDayTime){
-            imageBasedOnTimeofDay = 'img/day.svg';
-          } else {
-            imageBasedOnTimeofDay = 'img/night.svg';
-          }
-          time.setAttribute('src',imageBasedOnTimeofDay);
-
-
-          if(card.classList.contains('d-none')){
-            card.classList.remove('d-none');
-        
-        }
-
+  if (card.classList.contains("d-none")) {
+    card.classList.remove("d-none");
+  }
 };
 
-const updateCity = async (city) => {
-    
-    const cityDetails = await getCity(city);
-    const weather = await getWeather(cityDetails.Key);
+const fetchCityAndWeatherDetails = async (city) => {
+  const cityDetails = await getCity(city);
+  const weather = await getWeather(cityDetails.Key);
 
-    return { cityDetails, weather };
-
+  return { cityDetails, weather };
 };
 
-    cityForm.addEventListener('submit', e => {
-    e.preventDefault();
+cityForm.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-
-    const city = cityForm.city.value.trim();
-    cityForm.reset();
-    updateCity(city)
-    .then(data => updateUI(data))
-    .catch(err => console.log(err));
+  const city = cityForm.city.value.trim();
+  cityForm.reset();
+  fetchCityAndWeatherDetails(city)
+    .then((data) => updateUI(data))
+    .catch((err) => console.log(err));
 });
-
-
