620a8c767fa2d23f02823fb5648a3e9651a99d63
syedasarah10 <84844882+syedasarah10@users.noreply.github.com>
2021-06-16 15:28:07 +0530

    Add files via upload

---
+++ apikey.js
@@ -0,0 +1,1 @@
+export const WEATHER_API_KEY = "IvPdwpRySuANGGdlKPZscCcOw57G7CFL";
---
+++ index.js
@@ -0,0 +1,63 @@
+import {getWeather} from './app.js'
+import {getCity} from './app.js'
+const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
+
+
+const updateUI = (data) => {
+    const{cityDetails, weather } = data;
+    console.log(cityDetails);
+    console.log(weather);
+    
+
+    details.innerHTML = `
+          <h5 class="my-3">${cityDetails.EnglishName}</h5>
+          <div class="my-3">${weather.WeatherText}</div>
+          <div class="display-4 my-4">
+            <span>${weather.Temperature.Metric.Value}</span>
+            <span>&deg;C</span>
+          </div>
+          `;
+
+
+
+          let imageBasedOnTimeofDay = null;
+          if(weather.IsDayTime){
+            imageBasedOnTimeofDay = 'img/day.svg';
+          } else {
+            imageBasedOnTimeofDay = 'img/night.svg';
+          }
+          time.setAttribute('src',imageBasedOnTimeofDay);
+
+
+          if(card.classList.contains('d-none')){
+            card.classList.remove('d-none');
+        
+        }
+
+};
+
+const updateCity = async (city) => {
+    
+    const cityDetails = await getCity(city);
+    const weather = await getWeather(cityDetails.Key);
+
+    return { cityDetails, weather };
+
+};
+
+    cityForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+
+    const city = cityForm.city.value.trim();
+    cityForm.reset();
+    updateCity(city)
+    .then(data => updateUI(data))
+    .catch(err => console.log(err));
+});
+
+
